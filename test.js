6816c2b5db882dd11830fc09a1edb273486ba8a6
Hatemhusnieh <hatemhtm7@gmail.com>
2021-03-04 23:35:08 +0200

    Up Lab04

---
+++ test.js
@@ -0,0 +1,12 @@
+let color = prompt("what is your favorite colcor?");
+document.write("<h3> mmmmmm you said :</h3>" +"<h3>"+ color+"</h3>" + "<h3> !!! </h3>");
+if (color=="red") {
+    document.write("<p class= r>This is my favorite color too!!</p>")
+} else if (color =="black") {
+    document.write("<p class= y> This is my favorite color to wear</p>")
+} else if (color == "yellow") {
+    document.write("This is my GF favorite color too!!!")
+} else {
+    document.write("<p class= n> Bad choice :ppp</p>")
+}
+
