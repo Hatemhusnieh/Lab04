058569660f7f2be517ecd16acf7e12b509400fae
Hatemhusnieh <hatemhtm7@gmail.com>
2021-03-04 23:56:15 +0200

    Updating Lab04

--- test.js
+++ test.js
@@ -1,12 +1,17 @@
-let color = prompt("what is your favorite colcor?");
-document.write("<h3> mmmmmm you said :</h3>" +"<h3>"+ color+"</h3>" + "<h3> !!! </h3>");
+let color = prompt("what is your favorite color?");
+confirm("mmmm if it is not red, black, or yellow you can choose again");
+color = prompt("what is your favorite color Agan?");
+let name = prompt("OK now what is yor name:");
+confirm("ARE YOU SURE !!! LOL");
+name = prompt("I know!! I am too much now but name again please");
+document.write("<h3> mmmmmm you said: " + color+ " !!! </h3>");
 if (color=="red") {
-    document.write("<p class= r>This is my favorite color too!!</p>")
+    document.write("<p>OK "+name+"!!<br> This is my favorite color too!!</p>")
 } else if (color =="black") {
-    document.write("<p class= y> This is my favorite color to wear</p>")
+    document.write("<p>OK "+name+"!!<br> This is my favorite color to wear</p>")
 } else if (color == "yellow") {
-    document.write("This is my GF favorite color too!!!")
+    document.write("<p>OK "+name+"!!<br> This is my GF favorite color too!!!")
 } else {
-    document.write("<p class= n> Bad choice :ppp</p>")
+    document.write("<p> Bad choice "+name+" !!:ppp</p>")
 }
 
